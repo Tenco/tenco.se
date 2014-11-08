525d49cb99788b4348374466134a654c8cc572ff
Magnus Nilsson <magnus@tenco.se>
2014-11-08 21:55:32 +0100

    testing deploy hook

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Tenco AB"> 
     <link rel="shortcut icon" href="ico/favicon.png">
 
     <title>Tenco AB | Service Design</title>
